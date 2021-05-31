573acb15d8accc51f3395c06b4ce04592399e623
ThaiNgocLeDuy <64624794+ThaiNgocLeDuy@users.noreply.github.com>
2021-06-01 00:08:12 +0700

    add function search using fuse.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import { useState } from "react";
 import AddBookForm from "./components/forms/AddBookForm";
 import EditBookForm from "./components/forms/EditBookForm";
 import BookTable from "./components/tables/BookTable";
-import bookList from "./data";
+import bookList from "./components/data/data";
+import Fuse from "fuse.js";
 
 function App() {
+  //data
   const data = JSON.parse(localStorage.getItem("listbooks")) || bookList;
 
   const [books, setBooks] = useState(data);
 
+  //func add
   const addBook = (book) => {
     console.log(book);
     book.id = books.length + 1;
     const newBook = [...books, book];
     setBooks(newBook);
-
+    //set localstorage
     localStorage.setItem("listbooks", JSON.stringify(newBook));
   };
 
+  //func delete
   const deleteBook = (id) => {
     console.log(id);
     const index = books.findIndex((x) => x.id === id);
@@ -26,37 +30,64 @@ function App() {
     console.log(newBook);
     newBook.splice(index, 1);
     setBooks(newBook);
-
+    //set localstorage
     localStorage.setItem("listbooks", JSON.stringify(newBook));
   };
 
+  //isEdit to check whether click button edit
   const [isEdit, setIsEdit] = useState(false);
-
+  // initBook to set form
   const initBook = { id: null, name: "", author: "" };
-
+  //currentBook: book chosen
   const [currentBook, setCurrentBook] = useState(initBook);
 
+  //func editBook change to editForm
   const editBook = (id, book) => {
     setIsEdit(true);
     setCurrentBook(book);
   };
 
+  //func update book
   const updateBook = (updateBook) => {
     setBooks(
       books.map((book) => (book.id === currentBook.id ? updateBook : book))
     );
-
+    //find position of the book updated
     const index = books.findIndex((x) => x.id === updateBook.id);
     console.log(index);
-
     books[index] = updateBook;
-
     console.log(books);
 
     setCurrentBook(initBook);
     setIsEdit(false);
+    //set localstorage
+    localStorage.setItem("listbooks", JSON.stringify(books));
+  };
 
-    localStorage.setItem('listbooks', JSON.stringify(books));
+  //func search using fuse.js
+  //thanks to bit.dev
+  //you can learn about fuse in this link below
+  //https://blog.bitsrc.io/add-a-simple-search-function-to-react-app-without-a-server-22deda8966cd
+  const searchData = (keyword) => {
+    if (!keyword) {
+      setBooks(data);
+      return;
+    }
+
+    const fuse = new Fuse(data, {
+      keys: ["name", "author"],
+    });
+
+    const result = fuse.search(keyword);
+    const matches = [];
+    if (!result.length) {
+      setBooks([]);
+    } else {
+      result.forEach(({ item }) => {
+        matches.push(item);
+      });
+      setBooks(matches);
+    }
   };
 
   return (
@@ -81,7 +112,13 @@ function App() {
           )}
         </div>
         <div className="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-          <h2 className="text-center text-uppercase">List Books</h2>
+          <h2 className="text-uppercase d-inline">List Books</h2>
+          <input
+            className="rounded text-center float-right"
+            type="text"
+            placeholder="Enter to Search"
+            onChange={(e) => searchData(e.target.value)}
+          />
           <BookTable
             books={books}
             deleteBook={deleteBook}
--- src/components/tables/BookTable.jsx
+++ src/components/tables/BookTable.jsx
@@ -8,72 +8,75 @@ BookTable.propTypes = {
 };
 
 BookTable.defaultProps = {
-    books: [],
-    deleteBook: null,
-    editBook: null,
+  books: [],
+  deleteBook: null,
+  editBook: null,
 };
 
 function BookTable(props) {
-    const {deleteBook, editBook} = props;
+  const { books, deleteBook, editBook } = props;
 
-    const handleClickDeleteButton = (id) => {
-        console.log(id);
-        if (deleteBook) {
-            deleteBook(id);
-        }
+  const handleClickDeleteButton = (id) => {
+    console.log(id);
+    if (deleteBook) {
+      deleteBook(id);
     }
+  };
 
-    const handleClickEditButton = (id, book) => {
-        console.log(id);
-        console.log(book);
-        if (editBook) {
-            editBook(id, book);
-        }
+  const handleClickEditButton = (id, book) => {
+    console.log(id);
+    console.log(book);
+    if (editBook) {
+      editBook(id, book);
     }
+  };  
 
   return (
-    <table className="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th className="text-center">ID</th>
-          <th className="text-center">NAME</th>
-          <th className="text-center">AUTHOR</th>
-          <th className="text-center">ACTIONS</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.books.length > 0 ? (
-          props.books.map((book) => {
-            const { id, name, author } = book; //es6 syntax
-            return (
-              <tr key={id}>
-                <td className="text-center">{id}</td>
-                <td>{name}</td>
-                <td className="text-center">{author}</td>
-                <td className="text-center">
-                  <button 
-                    className="btn btn-danger mr-1"
-                    onClick={() => handleClickDeleteButton(id)}
-                  >
-                      DELETE
-                  </button>
-                  <button
-                   className="btn btn-warning"
-                   onClick={() => handleClickEditButton(id, book)}
-                   >
-                       EDIT
-                    </button>
-                </td>
-              </tr>
-            );
-          })
-        ) : (
+    <>
+      
+      <table className="table table-striped table-bordered">
+        <thead>
           <tr>
-            <td colSpan={8}>No Books found</td>
+            <th className="text-center">ID</th>
+            <th className="text-center">NAME</th>
+            <th className="text-center">AUTHOR</th>
+            <th className="text-center">ACTIONS</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {books.length > 0 ? (
+            books.map((book) => {
+              const { id, name, author } = book; //es6 syntax
+              return (
+                <tr key={id}>
+                  <td className="text-center">{id}</td>
+                  <td>{name}</td>
+                  <td className="text-center">{author}</td>
+                  <td className="text-center">
+                    <button
+                      className="btn btn-danger mr-1"
+                      onClick={() => handleClickDeleteButton(id)}
+                    >
+                      DELETE
+                    </button>
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => handleClickEditButton(id, book)}
+                    >
+                      EDIT
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan={8}>No Books found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
   );
 }
 
